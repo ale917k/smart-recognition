d2048efc8800c549978b4c2f94553e55633664d4
ale917k <alessiopetrin9@gmail.com>
2020-09-08 08:44:35 +0100

    Implemented Rank system using AWS Lambda

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Rank = ({ name, entries }) => {
+  const [emoji, setEmoji] = useState(null);
+
+  const generateEmoji = (entries) => {
+    fetch(
+      `https://kdqxu1a3qi.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`
+    )
+      .then((response) => response.json())
+      .then((data) => setEmoji(data.input))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    entries && generateEmoji(entries);
+  }, [entries]);
+
   return (
     <div>
       <div className="white f3">
         {`${name}, your current entry count is...`}
       </div>
       <div className="white f1">{entries}</div>
+      <div className="white f3">{`Rank Badge: ${emoji}`}</div>
     </div>
   );
 };
